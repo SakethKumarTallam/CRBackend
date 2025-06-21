8309e47e3b775454be01ef3f6eb8655703658fc2
SakethKumarTallam <sakethkumar594@gmail.com>
2025-06-22 01:06:23 +0530

    Update users.js

--- data/users.js
+++ data/users.js
@@ -1,19 +1,40 @@
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
+
+// const users = [
+//   {
+//     username: 'Admin User',
+//     password: bcrypt.hashSync('1234', 10),
+//     isAdmin: true,
+//   },
+//   {
+//     username: 'User 1',
+//     password: bcrypt.hashSync('1234', 10),
+//     isAdmin: false,
+//   },
+//   {
+//     username: 'User 2',
+//     password: bcrypt.hashSync('1234', 10),
+//     isAdmin: false,
+//   },
+// ];
+
+// module.exports = users;
+
 
 const users = [
   {
-    username: 'Admin User',
-    password: bcrypt.hashSync('1234', 10),
+    username: 'AdminManager',
+    password: 'Admin18',
     isAdmin: true,
   },
   {
-    username: 'User 1',
-    password: bcrypt.hashSync('1234', 10),
+    username: 'SakethKumar',
+    password: 'TSaketh01',
     isAdmin: false,
   },
   {
-    username: 'User 2',
-    password: bcrypt.hashSync('1234', 10),
+    username: 'AbhijeethM',
+    password: 'Abhi21',
     isAdmin: false,
   },
 ];
